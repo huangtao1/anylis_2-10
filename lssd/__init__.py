59ebc3e91cf16ed7e08deaa8281656657eac4f0e
huangtao1 <huangtao123689@gmail.com>
2018-01-16 15:15:09 +0800

    fix error

--- lssd/__init__.py
+++ lssd/__init__.py
@@ -19,6 +19,7 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.secret_key = 'huangtao123689mark!250$%#!'
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
