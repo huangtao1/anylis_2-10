e28a99d0751a8d67472c502964aa285799595c3f
huangtao1 <huangtao123689@gmail.com>
2018-01-16 14:26:55 +0800

    fix build error

--- lssd/main/views.py
+++ lssd/main/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Created by mark.huang on 2018/1/15.
-from . import main
+from lssd.main import main
 from flask import render_template, redirect, url_for, request, flash, current_app, jsonify, g, session, send_file
 from flask_login import login_user, logout_user, login_required, current_user
 
