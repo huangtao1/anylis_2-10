c3f18faf2ee22882a7e868e8ecb93e3c0d65c9d7
huangtao1 <huangtao123689@gmail.com>
2018-01-16 13:23:26 +0800

    modify file

--- lssd/__init__.py
+++ lssd/__init__.py
@@ -17,8 +17,8 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(Config[config_name])
-    Config[config_name].init_app(app)
+    app.config.from_object(Config.config_name)
+    Config.config_name.init_app(app)
 
     mail.init_app(app)
     moment.init_app(app)
